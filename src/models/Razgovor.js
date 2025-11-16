3e0059add523c40332ccd9576354cffcd855eed3
fcopcic <copcicf6@gmail.com>
2025-11-16 16:21:31 +0100

    Uklonjen backend za inbox modul

--- src/models/Razgovor.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const razgovorSchema = new mongoose.Schema({
-  korisnici: [{ type: mongoose.Schema.Types.ObjectId, ref: "Korisnik" }],
-  posljednjaPoruka: { type: String },
-  azurirano: { type: Date, default: Date.now },
-});
-
-export default mongoose.model("Razgovor", razgovorSchema);
